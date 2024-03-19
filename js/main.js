45399b85ead103c5988056e52f2f284a41bdd765
eeordonez <163948289+eeordonez@users.noreply.github.com>
2024-03-18 19:41:03 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,40 +1,4 @@
-/* let div_cronometro = document.querySelector(".cronometro");
-let tiempo;
 
-function iniciar_cronometro(minutos, segundo){
-    div_cronometro.innerHTML =`0${minutos}:${segundo}`
-
-    function actualizar (){
-        if (segundo==1 & minutos == 0){
-            clearInterval(tiempo);
-        }
-
-        segundo--;
-
-        //
-        if(segundo==0){
-            if(minutos>=1){
-                segundo = 60
-                minutos--;
-            }
-        }
-
-        div_cronometro.innerHTML ="";
-        
-        if(segundo < 10){
-            div_cronometro.innerHTML =`0${minutos}:0${segundo}`;
-        }else{
-            div_cronometro.innerHTML = `0${minutos}:${segundo}`;
-        }
-
-    }
-
-  
-    tiempo = setInterval(actualizar, 1000);
-}
-
-iniciar_cronometro(10, 11);
- */
 
 let div_cronometro = document.querySelector(".cronometro");
 let tiempo;
@@ -43,7 +7,7 @@ function iniciar_cronometro(minutos, segundos) {
     div_cronometro.innerHTML = `${minutos}:${segundos < 10 ? '0' : ''}${segundos}`;
 
     function actualizar() {
-        if (minutos === 1 && segundos === 0) {
+        if (minutos === 0 && segundos === 0) {
             clearInterval(tiempo);
             return;
         }
@@ -54,11 +18,19 @@ function iniciar_cronometro(minutos, segundos) {
             segundos = 59;
             minutos--;
         }
-
+        
         div_cronometro.innerHTML = `${minutos}:${segundos < 10 ? '0' : ''}${segundos}`;
+
     }
 
     tiempo = setInterval(actualizar, 1000);
 }
 
-iniciar_cronometro(10, 11);
+let Iniciar = document.createElement('button');
+        Iniciar.textContent = 'Iniciar';
+        Iniciar.addEventListener('click', function() {
+            iniciar_cronometro(10, 5); 
+        });
+        document.body.appendChild(Iniciar);
+
+
